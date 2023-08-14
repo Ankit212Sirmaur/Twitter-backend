60895db431ec35ac054145b5afea48c3b71df048
AnkitKumar <ankit17110747@yahoo.com>
2023-08-14 22:58:15 +0530

    databases updating properly but geeting old data

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const connect = require('./config/database');
 const app = express();
 
 const Tweet = require('./models/tweet');
+const TweetRepository = require('./repository/tweet-respository')
 
 app.listen(3000, async () =>{
     console.log('server started');
@@ -12,9 +13,11 @@ app.listen(3000, async () =>{
     // const  tweet = await Tweet.create({
     //     content: 'second tweet',
     // })
-    const tweet = await Tweet.findById('64da3758a8a7f2c8d3cc6129');
-    tweet.userEmail = 'b@c.com';
-    await tweet.save();
-    const tweet2 = await Tweet.find({userEmail: 'a@b.com '});
+    // const tweet = await Tweet.findById('64da3758a8a7f2c8d3cc6129');
+    // tweet.userEmail = 'b@c.com';
+    // await tweet.save(); 
+    // const tweet2 = await Tweet.find({userEmail: 'a@b.com '});
+    const tweetRepo = new TweetRepository();
+    const tweet = await tweetRepo.update('64da3569ce3bf5b8d4059250',{content: 'i am updating again'})
     console.log(tweet);
 });
---
+++ src/repository/tweet-respository.js
@@ -0,0 +1,38 @@
+const Tweet = require('../models/tweet');
+
+class TweetRepository {
+    async create(data){
+        try {
+            const tweet = await Tweet.create(data);
+            return tweet;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    async get(id){
+        try {
+            const tweet = await Tweet.findById(data);
+            return tweet;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    async update(tweetId, data){
+        try {
+            const tweet = await Tweet.findByIdAndUpdate(tweetId,data);
+            return tweet;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    async destroy(){
+        try {
+            const tweet = await Tweet.findById(data);
+            return tweet;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+module.exports = TweetRepository;
