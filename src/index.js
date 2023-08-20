0c8ea7bc543cbb6caf1626c7fa962bdac304ad1a
AnkitKumar <ankit17110747@yahoo.com>
2023-08-20 17:44:14 +0530

    Updated comment Model with refPath

--- src/index.js
+++ src/index.js
@@ -17,20 +17,6 @@ app.listen(3001, async () =>{
    console.log('SERVER STARTED');
     await connect();
     console.log('mongodb connected');
-
-    // const userRepo = new UserRepository();
-    // const tweetRepo = new TweetRepository();
-
-    // const tweets = await tweetRepo.getAll(0, 10);
-    // let tweet_id = tweets[0].id;
-    // // const user = await userRepo.create({
-    // //     email: 'ankit01@gmail.com',
-    // //     password: '212',
-    // //     name: 'kumar'
-    // // });
-    // const users = await userRepo.getAll();
-    // const likeService = new LikeService();
-    // await likeService.toogleLike(tweet_id, 'Tweet', users[0].id);
 });
 
 
