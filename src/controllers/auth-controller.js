331077f9165bf5407f0f844c7be1d90e46d1e77a
AnkitKumar <ankit17110747@yahoo.com>
2023-08-22 11:29:00 +0530

    added service for Login and Authentication

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -3,14 +3,14 @@ import UserService from "../service/user-service.js";
 const userService = new UserService();
 export const SignUp = async (req, res) =>{
     try {
-        const response = await userService.signUp({
+        const token = await userService.signUp({
             email: req.body.email,
             password: req.body.password,
             name: req.body.name
         });
         return res.status(200).json({
             success: true,
-            data: response,
+            data: token,
             err: {},
             message: 'Successfully Signup the user'
         })
@@ -21,5 +21,24 @@ export const SignUp = async (req, res) =>{
             message: 'Something went wrong while creating the new User',
             err: error, 
          })
+    }
+}
+
+export const login = async (req, res) =>{
+    try {
+        const token = await userService.singIn(req.body);
+        return res.status(200).json({
+            success: true,
+            message: 'Successfully logged in',
+            data: token,
+            err: {}
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Something went wrong',
+            data: {},
+            success: false,
+            err: error
+        });
     }
 }
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import {createTweet, getTweet} from '../../controllers/tweet-controller.js'
 import { toogleLike } from "../../controllers/like-controller.js";
 import { createComment } from "../../controllers/comment-controller.js";
-import { SignUp } from "../../controllers/auth-controller.js";
+import { SignUp, login } from "../../controllers/auth-controller.js";
+
+import { authenticate } from "../../middlewares/authenticate.js";
 
 const router  = express.Router();
  
-router.post('/tweets', createTweet);
+router.post('/tweets',authenticate ,createTweet);
 
 router.post('/likes/toggle', toogleLike);
 
@@ -15,5 +17,7 @@ router.post('/comments', createComment);
 router.get('/tweets/:id',  getTweet);
 
 router.post('/signup',  SignUp);
+
+router.post('/login', login);
 
 export default router;
